63c0c63f1b29aed966f4cf1078bef3f7d9b7b2a2
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-12 19:14:02 +0100

    django

---
+++ demo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+# localhost:8080/demo/hello
+
+def say_hello(request):
+    return HttpResponse("Welcome to Django Demo! ")
+
+
+def welcome(request, name):
+   return render(request, "index.html",{"name": name})
